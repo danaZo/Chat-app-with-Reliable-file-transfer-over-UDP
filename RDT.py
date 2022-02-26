368a7a79634479a40137d221a5754defd66525b7
noam vanunu <noamv23@gmail.com>
2022-02-26 13:43:33 +0200

    minor fixes

---
+++ RDT.py
@@ -0,0 +1,10 @@
+import socket
+
+'''
+this module adds reliable data transfer attributes to the the unreliable
+UDP connection.
+'''
+
+
+def rdtSend(sender: socket.socket, destination: (str, int), msg: str):
+    pass
