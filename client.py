6c6199233f62565a73516ccc7f6e5deabc42abb2
danaZo <93203695+danaZo@users.noreply.github.com>
2022-03-04 13:39:03 +0200

    gui with file handling

--- client.py
+++ client.py
@@ -1,27 +1,33 @@
+import pickle
 import signal
 import socket
 import sys,os
-import threading
+import time
 from threading import Thread,main_thread
 import socket
 from tkinter import *
 from tkinter import font
 from tkinter import ttk
+import tkinter.messagebox
+
+ACK_REQ = 10000000
+STOP_REQ = 20000000
+fSizeInPkts: int
+fname: str
 
 is_windows = sys.platform.startswith('win')
 serverPort = 50000
 serverIp = socket.gethostname()
-menu = "WELCOME!\nAt the left side box, you can type the following commands:\n" \
+menu = "WELCOME!\n\nAt the left side box, you can type the following commands:\n" \
        "all : To send a public message\n" \
        "member's name : To send a private message to specific member (write the member's name)\n" \
        "quit : To quit the chat\n" \
-       "online : To get a list of online members names"
+       "online : To get a list of online members names\n" \
+       "getfiles : To get the list of files that able to be downloaded\n" \
+       "file : To download a file from the existing files"
 FORMAT = "utf-8"
 clientSoc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-
-
-
+fileSoc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 """
 In this class we have the chat GUI from the client's side
@@ -58,18 +64,18 @@ class GUI:
         # Call the mainloop of Tk.
         self.Window.mainloop()
 
-    def connectToServer(self, soc: socket.socket):
+    def connectToServer(self, soc: socket.socket, udp: socket.socket):
         # connecting to the server
 
         try:  # set connection and user name
             response = b''
             soc.connect((serverIp, serverPort))
+            udp.bind((socket.gethostname(), soc.getsockname()[1]))
             while response.decode() != "NAME_OK":
                 print(response.decode())
                 name = self.name
                 soc.send(name.encode(FORMAT))
                 response = soc.recv(128)
-
 
         except Exception as e:
             print(f"Connection failed due to: {e}")
@@ -80,9 +86,9 @@ class GUI:
         self.login.destroy()
         self.layout(name)
         # initialize a daemon thread to listen for incoming messages
-        self.connectToServer(clientSoc)
+        self.connectToServer(clientSoc, fileSoc)
         self.textCons.config(state=NORMAL)
-        self.textCons.insert(END,"Connected successfully" + "\n\n")
+        self.textCons.insert(END, "Connected successfully" + "\n\n")
         self.textCons.insert(END, menu + "\n\n")
         self.textCons.config(state=DISABLED)
         self.textCons.see(END)
@@ -95,32 +101,40 @@ class GUI:
         self.Window.deiconify()
         self.Window.title("ChatRoom")
         self.Window.resizable(width=True, height=True)
-        self.Window.configure(width=900, height=550, bg="#17202A")
+        self.Window.configure(width=900, height=800, bg="#17202A")
 
+        # the label with the client's name
         self.labelHead = Label(self.Window, bg="#17202A", fg="#EAECEE", text=self.name, font="Helvetica 13 bold", pady=5)
         self.labelHead.place(relwidth=1)
 
         self.line = Label(self.Window, width=450, bg="#ABB2B9")
         self.line.place(relwidth=1, rely=0.07, relheight=0.012)
 
+        # the place where text appears
         self.textCons = Text(self.Window, width=20, height=2, bg="#17202A", fg="#EAECEE", font="Helvetica 14", padx=5, pady=5)
         self.textCons.place(relheight=0.745, relwidth=1, rely=0.08)
 
         self.labelBottom = Label(self.Window, bg="#ABB2B9", height=80)
         self.labelBottom.place(relwidth=1, rely=0.925)
 
-        self.labelDown = Label(self.Window, bg="#17202A", fg="#EAECEE", text="Type: all/online/\nmember's name/quit",
+        # the text above the smaller box
+        self.labelDown = Label(self.Window, bg="#17202A", fg="#EAECEE",
+                               text="Commands: all/online/\nmember's name/quit/\ngetfiles/file",
                                font="Helvetica 13 bold", pady=6)
         self.labelDown.place(relwidth=0.4 ,rely=0.825 , relx=-0.05)
 
-        self.labelMsg = Label(self.Window, bg="#17202A", fg="#EAECEE", text="Type message (blank to online/quit commands)",
+        # the text above the bigger box
+        self.labelMsg = Label(self.Window, bg="#17202A", fg="#EAECEE",
+                              text="Type message/file name (blank to online/quit/getfiles commands)",
                                font="Helvetica 13 bold", pady=6)
         self.labelMsg.place(relwidth=0.6, rely=0.85, relx=0.25)
 
+        # the small box
         self.entryType = Entry(self.labelBottom, bg="#2C3E50", fg="#EAECEE", font="Helvetica 13")
         self.entryType.place(relwidth=0.26, relheight=0.03, rely=0.0, relx=0.011)
         self.entryType.focus()
 
+        # the big box
         self.entryMsg = Entry(self.labelBottom, bg="#2C3E50", fg="#EAECEE", font="Helvetica 13")
         self.entryMsg.place(relwidth=0.74, relheight=0.03, rely=0.0, relx=0.3)
         self.entryMsg.focus()
@@ -146,8 +160,7 @@ class GUI:
         self.msg = msg
         self.entryMsg.delete(0, END)
         self.entryType.delete(0, END)
-        self.sendMessage(clientSoc)
-
+        self.sendMessage(clientSoc, fileSoc)
 
     def getMessages(self, soc: socket.socket):
         while True:
@@ -176,13 +189,16 @@ class GUI:
                 soc.close()
                 sys.exit()
 
-    def sendMessage(self, soc: socket.socket):
+    def sendMessage(self, soc: socket.socket,fileSoc: socket.socket ):
         print(menu)
         while True:
             message = self.msg
 
             if message == "quit:":
                 return
+
+            if message[:4] == 'file':
+                Thread(target=self.getFile, daemon=True, args=(fileSoc,)).start()
 
             try:
                 soc.send(message.encode(FORMAT))
@@ -192,15 +208,95 @@ class GUI:
                 sys.exit()
 
 
+    def getFile(self, fileSoc: socket.socket):
+        # get file name and the number of incoming packets
+        # file names to text box
+        self.textCons.config(state=NORMAL)
+        self.textCons.insert(END, "Incoming file" + "\n\n")
+        self.textCons.config(state=DISABLED)
+        self.textCons.see(END)
+        print(fileSoc)
+        print("Incoming file")
+
+        filename, _ = fileSoc.recvfrom(128)
+        # print to chat the file name
+        self.textCons.config(state=NORMAL)
+        self.textCons.insert(END, str(filename) + "\n\n")
+        self.textCons.config(state=DISABLED)
+        self.textCons.see(END)
+        print(filename)
+
+        packetsNum, _ = fileSoc.recvfrom(128)
+        packetsNum = int(packetsNum.decode())
+
+        # print to chat the number of packets
+        self.textCons.config(state=NORMAL)
+        self.textCons.insert(END, "size in packets: " + str(packetsNum) + "\n\n")
+        self.textCons.config(state=DISABLED)
+        self.textCons.see(END)
+        print("size in packets: " + str(packetsNum))
+
+        packets = [b''] * packetsNum  # list to save the received packets
+
+        fileSoc.settimeout(1)  # close thread and socket if no data sent for 5 seconds
+        f = open(filename.decode(), 'wb')
+        Acks = []
+        try:
+            while True:
+
+                # get packets from server
+                pac, addr = fileSoc.recvfrom(2048)
+                # check if the pac is part of the file or it is an ack request
+                seqNo = int.from_bytes(pac[:4], byteorder='big')
+                if seqNo == ACK_REQ:
+                    # we send the ACK list to the server
+                    fileSoc.sendto(pickle.dumps(Acks), addr)
+                    continue
+
+                if seqNo == STOP_REQ:
+                    # create pop up window asking the user to resume the download
+                    res = tkinter.messagebox.askquestion('file download', 'Would you like to continue downloading?')
+                    if res == 'yes':
+                        continue
+
+                # check if we got the package already
+
+                if packets[seqNo] == b'':  # packet haven't buffered before - solves problem of duplicate packets
+                    packets[seqNo] = pac[4:]
+
+                    self.textCons.config(state=NORMAL)
+                    self.textCons.insert(END, str(seqNo) + "\n\n")
+                    self.textCons.config(state=DISABLED)
+                    self.textCons.see(END)
+                    print(seqNo)
+                    Acks.append(seqNo)  # mark that we got this package
+
+                if b'' not in packets:
+                    self.textCons.config(state=NORMAL)
+                    self.textCons.insert(END, "all packets received successfully" + "\n\n")
+                    self.textCons.config(state=DISABLED)
+                    self.textCons.see(END)
+                    print("all packets received successfully")
+                    # read the last ACK_REQ to clear the buffer
+                    d, _ = fileSoc.recvfrom(128)
+                    self.textCons.config(state=NORMAL)
+                    self.textCons.insert(END, f"leftovers {d}" + "\n\n")
+                    self.textCons.config(state=DISABLED)
+                    self.textCons.see(END)
+                    print(f"leftovers {d}")
+                    break
+
+            for pac in packets:
+                f.write(pac)
+            f.close()
+            # flush remaining data
+            fileSoc.settimeout(0.1)
+        except socket.error:
+            f.close()
+
+
 if __name__ == '__main__':
     g = GUI()
 
-    #print("Connected successfully")
-
-    # initialize a daemon thread to listen for incoming messages
-    #Thread(target=getMessages, daemon=True, args=(clientSoc,)).start()
-
-    # send messages when user want to
-    #sendMessage(clientSoc)
 
 
--- server.py
+++ server.py
@@ -1,21 +1,32 @@
 import socket
 from threading import Thread
-import sys
+import sys, os
+import pickle
+
+import time
 
 MSG_HDR = 10
 serverPort = 50000
 serverIP = socket.gethostname()
 clients: {socket.socket}
 clients = dict()
-forbbidenNames = ["all", "quit", "online"]
+clientAddr = dict()
+clientFileSoc = dict()
+forbbidenNames = ["all", "quit", "online", "file"]
+ACK_REQ = 10000000
+STOP_REQ = 20000000
 serverSocket: socket.socket
+fileSoc: socket.socket
 
 
 def setUpServer():
-    global serverSocket
+    global serverSocket, fileSoc
     # server initialization
     try:
         serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        fileSoc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        fileSoc.bind((socket.gethostname(), 50000))
+
     except socket.error as e:
         print(f"Error opening socket: {e}")
         sys.exit()
@@ -48,6 +59,8 @@ def shutDown():
     for soc in clients:
         soc.close()
         sys.exit()
+    fileSoc.close()
+    serverSocket.close()
 
 
 def getOnlineUsers(client: socket.socket):
@@ -55,6 +68,92 @@ def getOnlineUsers(client: socket.socket):
     for name in clients.keys():
         res = res + name + ', '
     client.send(f"Users Online: {res[:-2]}".encode())
+
+
+def getFilesOnserver(client: socket.socket, name: str):
+    directMessage("Available files: ", name, str(os.listdir('Files')))
+
+
+def sendPkstByNums(indices: list, packetList: list, fileSoc: socket.socket, addr):
+    # send the number of packets to send
+    fileSoc.settimeout(1)
+    print(f"Sending {len(indices)} packets")
+
+    boolVal = True
+    packetToLoose = [1, 3, 5, 8]
+
+    while indices:
+        for i in indices:
+            if i == 10 and boolVal:
+                boolVal = False
+                continue
+            fileSoc.sendto(packetList[i], addr)
+        # eliminate the Ack'ed packets from the list
+        time.sleep(0.15)
+
+
+        fileSoc.sendto(ACK_REQ.to_bytes(4, 'big'), addr)
+        data, _ = fileSoc.recvfrom(1024)
+        acked = pickle.loads(data)  # contain the Acked packets
+        indices = [x for x in indices if x not in acked]
+        if len(indices) == 0:
+            print("all packets received successfully")
+            break
+
+
+        print("retransmiting packets: " + str(indices))
+
+    print(f"all {len(indices)} packets ACK'ed")
+
+
+def sendFile(client: socket.socket, filename: str, client_name: str):
+    global fileSoc
+
+    # open the requested file if it exits
+    packetList = []
+    pacNo = 0
+
+    filename = 'Files/' + filename
+
+    try:
+
+        f = open(filename, 'rb')
+
+
+    except IOError:
+        client.send("File does not exits".encode())
+        return
+
+    # split the file into packets of 1350 Bytes
+
+    while True:
+        segment = f.read(1350)
+        if segment == b'':
+            break
+        segment = pacNo.to_bytes(4, 'big') + segment  # add the serial number of the packet
+        packetList.append(segment)  # store the file as list of ordered packets
+        pacNo += 1
+    f.close()
+    # open UDP socket for the server
+    try:
+        # send the name of the file
+        fileSoc.sendto(filename[6:].encode(), (socket.gethostname(), clientAddr[clientSoc][1]))
+        # send the number of packets that were extracted from the file
+        fileSoc.sendto(str(pacNo).encode(), (socket.gethostname(), clientAddr[clientSoc][1]))
+
+        # send the packets
+        addr = (socket.gethostname(), clientAddr[clientSoc][1])
+        # sendPkstByNums(list(range(pacNo)), packetList, fileSoc, addr)
+        sendPkstByNums(list(range(int(pacNo/2))), packetList, fileSoc,addr )
+        #fileSoc.sendto(STOP_REQ.to_bytes(4, byteorder="big"), addr)
+        sendPkstByNums(list(range(int(pacNo / 2), pacNo)), packetList, fileSoc,addr)
+
+
+
+
+    except socket.error as e:
+        print(f"Could not send the file {e}")
+        return
 
 
 def broadcast(name: str, msg: str):
@@ -115,11 +214,13 @@ def listenToClient(clientSoc: socket.socket, name: str):
     :param clientSoc: the costumer
     :param name: client's name
     """
+
     # listening to messages from the client
     while True:
+
         try:
             msg = clientSoc.recv(1024).decode()
-
+            print("command: " + msg)
             if len(msg) == 0:
                 disconnectClient(clientSoc, name)
                 return
@@ -128,16 +229,24 @@ def listenToClient(clientSoc: socket.socket, name: str):
             if msg[0] == "all":
                 broadcast(name, ''.join(msg[1:]))
                 continue
+
             if msg[0] == "quit":
-                disconnectClient(clientSoc,name)
+                disconnectClient(clientSoc, name)
                 return
+
             if msg[0] in clients.keys():
                 directMessage(name, msg[0], ''.join(msg[1:]))
+                continue
+
+            if msg[0] == 'file':
+                sendFile(clientSoc, msg[1], name)
                 continue
 
             if msg[0] == 'online':
                 getOnlineUsers(clientSoc)
                 continue
+            if msg[0] == 'getfiles':
+                getFilesOnserver(clientSoc, name)
             else:
                 clientSoc.send("Unknown user or command ".encode())
 
@@ -150,6 +259,7 @@ def listenToClient(clientSoc: socket.socket, name: str):
 setUpServer()
 while True:
     clientSoc, caddr = serverSocket.accept()
+    clientAddr[clientSoc] = caddr
     print(f"{caddr[1]} connected to the server")
     client_name = connectNewClient(clientSoc)
     # assign the client its own thread and send him off
